bbeefae305e6f7d1dc55aec5f83226703612ee60
curtis chat <curtis.ch.chat@gmail.com>
2024-04-05 14:26:18 -0600

    update initializer layout

--- pycalc.py
+++ pycalc.py
@@ -1,3 +1,43 @@
+# pycalc.py
+
+# pycal is a simple calculator build with python and qt
+
 import sys
 
-from PyQt5.QtWidgets import QApplication, QMain
+from Pyqt5.QtCore import Qt
+from PyQt5.QtWidgets import (
+    QApplication,
+    QGridLayout,
+    QLineEdit,
+    QMainWindow,
+    QPushButton,
+    QVBoxLayout,
+    Qwidget,
+)
+
+WINDOW_SIZE = 235
+
+class PyCalcWindow(QMainWindow):
+    # PyCalc's main window (GUI or view).
+
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("PyCalc")
+        self.setFixedSize(WINDOW_SIZE, WINDOW_SIZE)
+        self.generalLayout = QVBoxLayout()
+        centralWidget = QWidget(self)
+        centralWidget.setLayout(self.generalLayout)
+        self.setCentralWidget(centralWidget)
+        self._createDisplay()
+        self._createButtons()
+
+def main():
+    # pycalc's main function best practice to use main()
+    pycalcApp = QApplication([])
+    pycalcWindow = PyCalcWindow()
+    pycalcWindow.show()
+    sys.exit(pycalcApp.exec())
+
+if __name__ == "__main__":
+    main()
+
