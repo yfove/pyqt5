2054cf7d43baccb5de7c535a10c02664f54b6024
curtis chat <curtis.ch.chat@gmail.com>
2024-04-05 16:36:27 -0600

    controller class setup

--- pycalc.py
+++ pycalc.py
@@ -3,7 +3,7 @@
 # pycal is a simple calculator build with python and qt
 
 import sys
-
+from functools import partial
 from Pyqt5.QtCore import Qt
 from PyQt5.QtWidgets import (
     QApplication,
@@ -15,7 +15,10 @@ from PyQt5.QtWidgets import (
     Qwidget,
 )
 
+ERROR_MSG = "ERROR"
 WINDOW_SIZE = 235
+DISPLAY_HEIGHT = 35
+BUTTON_SIZE = 40 
 
 class PyCalcWindow(QMainWindow):
     # PyCalc's main window (GUI or view).
@@ -31,6 +34,76 @@ class PyCalcWindow(QMainWindow):
         self._createDisplay()
         self._createButtons()
 
+    # setting up display dimensions and read only
+    def _createDisplay(self):
+        self.display = QLineEdit()
+        self.display.setFixedHeight(DISPLAY_HEIGHT)
+        self.display.setAlignment(Qt.AlignmentFlag.AlignRight)
+        self.display.setReadOnly(True)
+        self.generalLayout.addWidget(self.display)
+
+    # create empty directory to hold buttons then create array to store labels
+    def _createButtons(self):
+        self.buttonMap = {}
+        buttonsLayout = QGridLayout()
+        keyBoard =[
+            ["7", "8", "9", "/", "C"],
+            ["4", "5", "6", "*", "("],
+            ["1", "2", "3", "-", ")"],
+            ["0", "00", ".", "+", "="],
+        ]
+
+        # outer loop iterate over the rows and inner columns
+        # creates buttons and add then to map and layout
+        for row, keys in enumerate(keyBoard):
+            for col, key in enumerate(keys):
+                self.buttonMap[key] = QPushButton(key)
+                self.buttonMap[key].setFixedSize(BUTTON_SIZE, BUTTON_SIZE)
+                buttonsLayout.addWidget(self.buttonMap[key], row, col)
+
+        # embed the grid into general layout
+        self.generalLayout.addLayout(buttonsLayout)
+
+        def setDisplayText(self, text):
+            # set the display's text
+            self.display.setText(text)
+            self.display.setFocus()
+
+        def displayText(self):
+            # get the display's text
+            return self.display.text()
+
+        def clearDisplay(self):
+            # clear the display
+            self.setDisplayText("")
+
+class PyCalc:
+    # pycalc's controller class
+    def __init__(self, model, view):
+        self._evaluate = model
+        self._view = view
+        self._connectSignalsAndSlots()
+
+    def _calculateResult(self):
+        result = self._evaluate(expression=self._view.displayText())
+        self._view.setDisplayText(result)
+    
+    def _buildExpressions(self, subExpression):
+        if self._view.displayText() == ERROR_MSG:
+            self._view.clearDisplay()
+        expression = self._view.displayText() + subExpression
+        self._view.setDisplayText(expression)
+
+    def _connectSignalsAndSlots(self):
+        for keySymbol, button in self._view.buttonMap.item():
+            if keySymbol not in {"=", "C"}:
+                button.clicked.connect(
+                    partial(self._buildExpression, keySymbol)
+                )
+        self._view.buttonMap["="].clicked.connect(self._calculateResult)
+        self._view.display.returnPressed.connect(self._calculateResult)
+        self._view.buttonMap["C"].clicked.connect(self._view.clearDisplay)
+
 def main():
     # pycalc's main function best practice to use main()
     pycalcApp = QApplication([])
@@ -38,6 +111,13 @@ def main():
     pycalcWindow.show()
     sys.exit(pycalcApp.exec())
 
+def evaluateExpression(expression):
+    # evaluate an expression (Model)
+    try:
+        result = str(eval(expression, {}, {}))
+    except Exception:
+        result = ERROR_MSG
+    return result
+
 if __name__ == "__main__":
     main()
-
